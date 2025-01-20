5d125efb1e2c205c3ce6cc681effc7f5852cc246
balshammri04 <bayanalshammri7@gmail.com>
2025-01-20 14:42:31 +0300

    Initial Commit

---
+++ Movieisme/MovieismeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieismeApp.swift
+//  Movieisme
+//
+//  Created by bayan alshammri on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieismeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
